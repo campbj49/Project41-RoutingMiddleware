30ef2ae0cd601a017b08d51af952453329497639
John Campbell <campbj49@gmail.com>
2023-08-23 16:54:58 -0400

    finish specific item routes

--- items.js
+++ items.js
@@ -27,16 +27,45 @@ router.post("/", function(req, res, next){
 /** GET /items/:name get specific item details */
 router.get("/:name" , function(req, res, next) {
   //find item name in list, throwing an error if the item isn't in the list
-  let name = req.params.name;
-  let itemFound;
-  for(item of items){
-    if(item.name == name) itemFound=item;
-  }
+  let itemFound = items.find((element) => element.name === req.params.name);
+  
   if(!itemFound){
     let error = new ExpressError(`Item not found`, 400)
     return next(error);
   }
-  return res.json(item);
+  return res.json(itemFound);
+});
+
+/** PATCH /items/:name update specific item details */
+router.patch("/:name" , function(req, res, next) {
+  //find item name in list, throwing an error if the item isn't in the list
+  let itemFound = items.find((element) => element.name === req.params.name);
+  if(!itemFound){
+    let error = new ExpressError(`Item not found`, 400)
+    return next(error);
+  }
+  //update item and send succesful message
+  itemFound.name = req.query.name;
+  itemFound.price = req.query.price;
+
+  return res.json({updated:itemFound});
+});
+
+/** DELETE /items/:name delete specific item details */
+router.delete("/:name" , function(req, res, next) {
+  //throw error if list is empty
+  if(!items.length) return next(new ExpressError('List is empty', 400))
+
+  //find item name in list, throwing an error if the item isn't in the list
+  let indexFound = items.findIndex((element) => element.name === req.params.name);
+  if(indexFound === -1){
+    let error = new ExpressError(`Item not found`, 400)
+    return next(error);
+  }
+  //delete item and send succesful message
+  items.splice(indexFound);
+
+  return res.json({message:"Deleted"});
 });
 
 
